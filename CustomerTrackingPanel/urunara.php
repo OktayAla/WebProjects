a8ff73ed69411969fae1e9289c741f87cc5898de
Oktay Ala <oktayala@gmail.com>
2025-09-11 00:08:59 +0300

    Update urunara.php

--- CustomerTrackingPanel/urunara.php
+++ CustomerTrackingPanel/urunara.php
@@ -1,4 +1,7 @@
-<?php
+<!-- Bu sistem Oktay ALA tarafından, Analiz Tarım için geliştirilmiştir. -->
+<!-- Copyright © Her Hakkı Saklıdır. Ticari amaçlı kullanılması yasaktır. -->
+ 
+<?php 
 require_once __DIR__ . '/includes/auth.php';
 require_login();
 
