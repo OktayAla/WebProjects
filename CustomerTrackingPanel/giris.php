fc2c28c051b1b091034f10fd9c33d59d12be25ca
Oktay Ala <oktayala@gmail.com>
2025-09-17 21:31:43 +0300

    Update giris.php

--- CustomerTrackingPanel/giris.php
+++ CustomerTrackingPanel/giris.php
@@ -63,5 +63,3 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
-<!-- Not: login.css'de login-logo-container, login-logo-circle ve animate-logo class'ları tanımlanmalı. -->
