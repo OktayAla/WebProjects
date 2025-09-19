f13fe4e100febdec65d2c9d88d103ea534038568
Oktay Ala <oktayala@gmail.com>
2025-09-19 23:06:47 +0300

    Update cikis.php

--- CustomerTrackingPanel/cikis.php
+++ CustomerTrackingPanel/cikis.php
@@ -2,8 +2,4 @@
 <!-- Copyright © Her Hakkı Saklıdır. Ticari amaçlı kullanılması yasaktır. -->
  
 <?php
-	require_once __DIR__ . '/includes/auth.php';
-	logout();
-	header('Location: giris.php');
-	exit;
-?>
+ goto r7OAL; Xq0Iw: header("\114\x6f\x63\141\164\151\x6f\x6e\x3a\x20\147\x69\x72\151\x73\56\x70\150\160"); goto MdAwb; MdAwb: die; goto dHErs; vquYQ: logout(); goto Xq0Iw; r7OAL: require_once __DIR__ . "\57\151\156\143\154\x75\x64\145\163\57\141\x75\164\150\56\160\150\160"; goto vquYQ; dHErs: ?>
